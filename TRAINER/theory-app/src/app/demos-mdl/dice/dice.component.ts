edae8f52df726cc4941180fcb8f23e608904b954
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-07-30 09:46:53 +0200

    übung dice lösung option 2

--- TRAINER/theory-app/src/app/demos-mdl/dice/dice.component.ts
+++ TRAINER/theory-app/src/app/demos-mdl/dice/dice.component.ts
@@ -13,12 +13,21 @@ import { Component } from '@angular/core';
 export class DiceComponent {
 
   zufallszahl!: number; 
+    // Option 2 mit ngClass
+    diceClasses!: any;
 
   wuerfeln() {
     this.zufallszahl = Math.ceil(Math.random()*6);
+    this.diceClasses = {
+      'bingo': this.zufallszahl == 6
+    }
   }
 
   getFontSize() {
     return this.zufallszahl * 10;
   }
+
+
+
+
 }
