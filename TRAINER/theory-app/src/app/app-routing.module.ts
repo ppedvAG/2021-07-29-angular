4f1545c2d71c201cc1adebfdb7afdcf388b7d4ab
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-07-30 14:33:30 +0200

    pw confirm lösung

--- TRAINER/theory-app/src/app/app-routing.module.ts
+++ TRAINER/theory-app/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { EventsComponent } from './demos-mdl/events/events.component';
 import { InputVarsComponent } from './demos-mdl/input-vars/input-vars.component';
 import { LifecycleComponent } from './demos-mdl/lifecycle/lifecycle.component';
 import { OverviewDemosComponent } from './demos-mdl/overview-demos/overview-demos.component';
+import { PwconfirmComponent } from './demos-mdl/pwconfirm/pwconfirm.component';
 import { TemplateVarsComponent } from './demos-mdl/template-vars/template-vars.component';
 import { TimeComponent } from './demos-mdl/time/time.component';
 import { OverviewTodosComponent } from './todos-mdl/overview-todos/overview-todos.component';
@@ -24,6 +25,7 @@ const routes: Routes = [
       {path: 'lifecycle', component: LifecycleComponent},
       {path: 'template-vars', component: TemplateVarsComponent},      
       {path: 'time', component: TimeComponent},     
+      {path: 'app-pwconfirm', component: PwconfirmComponent},     
     ]
   },
   {path: 'todos', component: OverviewTodosComponent}
