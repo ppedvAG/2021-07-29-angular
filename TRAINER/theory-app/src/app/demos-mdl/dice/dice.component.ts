f3980288d87f0498fdb924ff2defa913c72ffe8c
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-07-30 09:41:14 +0200

    übung dice

--- TRAINER/theory-app/src/app/demos-mdl/dice/dice.component.ts
+++ TRAINER/theory-app/src/app/demos-mdl/dice/dice.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dice',
   templateUrl: './dice.component.html',
-  styles: [
+  styles: [`
+    .bingo {
+    color: red;
+    background-color: blueviolet;
+}`
   ]
 })
-export class DiceComponent implements OnInit {
+export class DiceComponent {
 
-  constructor() { }
+  zufallszahl!: number; 
 
-  ngOnInit(): void {
+  wuerfeln() {
+    this.zufallszahl = Math.ceil(Math.random()*6);
   }
 
+  getFontSize() {
+    return this.zufallszahl * 10;
+  }
 }
